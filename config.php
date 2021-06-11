38a922e6c06a885da22e16b7aac9053cdd96b5a0
shauqi <shauqi>
2021-06-11 02:11:32 -0700

    4

--- config.php
+++ config.php
@@ -1,16 +1,16 @@
 <?php
 
-$servername = "localhost";
+$servername = "127.0.0.1";
 $username = "root";
 $password = "";
+$db_name = "student";
 
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
-  } catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-  }
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $db_name);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
 
 ?>
---
+++ controller/auth.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+
+if(isset($_SESSION["login"]) || $_SESSION["login"] == "true") {
+    header("location: ../index.php");
+    exit;
+}
+
+require_once("../config.php");
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = trim($_POST['email']);
+    $password = sha1(trim($_POST['password']));
+
+    $query = "SELECT * FROM `users` WHERE email='" . $email . "' AND password='" . $password . "'";
+    echo $query;
+    $result = mysqli_query($conn, $query);
+
+    if(mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        
+        $_SESSION["name"] = $row["name"];
+        $_SESSION["login"] = "true";
+
+        header("location: ../index.php");
+        exit;
+    }else {
+        header("location: ../login.php");
+        exit;
+    }
+}
+
+?>
