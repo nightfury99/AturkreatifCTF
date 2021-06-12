12a0955e5264bc1d592e3883678699b38519007f
shauqi <shauqi>
2021-06-12 06:53:03 -0700

    13. fix dollar

--- dashboard.php
+++ dashboard.php
@@ -371,7 +371,7 @@ require_once("594c103f2c6e04c3d8ab059f031e0c1a/session.php");
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                                 Earnings (Monthly)</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">RM40,000</div>
                                         </div>
                                         <div class="col-auto">
                                             <i class="fas fa-calendar fa-2x text-gray-300"></i>
@@ -389,7 +389,7 @@ require_once("594c103f2c6e04c3d8ab059f031e0c1a/session.php");
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
                                                 Earnings (Annual)</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">RM215,000</div>
                                         </div>
                                         <div class="col-auto">
                                             <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
